4fc8b2029b82b10806a01d86547cfbe32032314f
RunranLi <2271704437@qq.com>
2022-07-07 16:27:45 -0700

    HUD

---
+++ ProjectTemp-Unity/Assets/Scripts/HUDScript.cs
@@ -0,0 +1,39 @@
+﻿/****
+* Created by: Runran Li
+* Date Created: July 6, 2022
+*
+* Last Edited by:
+* Last Edited:
+*
+* Description: Manages the UI elements for the menu scenes
+****/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //libraries for UI components
+using TMPro; //libraries for TextMeshPro components
+
+public class HUDScript : MonoBehaviour
+{
+    [Header("Stats Placement")]
+    public TMP_Text levelCountTextbox; //textbox for level count
+    public TMP_Text livesTextbox; //textbox for the lives
+    public TMP_Text healthTextbox; //textbox for highscore
+    public TMP_Text scoreTextbox; //textbox for the score
+    public TMP_Text highScoreTextbox; //textbox for highscore
+    public TMP_Text collectableCountTextbox; //textbox for amount of collectables
+    public TMP_Text TimerTextbox; //textbox for Timer display
+    public TMP_Text fastestTimeTextbox; //textbox for the Fastest Time
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ ProjectTemp-Unity/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,45 @@
+﻿/****
+* Created by: Runran Li
+* Date Created: July 6, 2022
+*
+* Last Edited by:
+* Last Edited:
+*
+* Description: Manages the UI elements for the menu scenes
+****/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; //libraries for UI components
+using TMPro; //libraries for TextMeshPro components
+
+public class MenuScript : MonoBehaviour
+{
+    [Header("Text Boxes")]
+    public TMP_Text titleTextbox; //textbox for the title
+    public TMP_Text creditsTextbox; //textbox for the credits
+    public TMP_Text copyrightTextbox; //textbox for the copyright
+    public TMP_Text mesageTextbox; //textbox for end message
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //set the value for textboxes
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnGameStart()
+    {
+        Debug.Log("Game Started");
+    }//end OnGameStart()
+
+    public void OnGameExit()
+    {
+        Debug.Log("Exited Game");
+    }//end OnGameExit()
+
+}
